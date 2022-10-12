ca34bc77410e052face655ad852f5cae4a819632
shekhar <shekharmishra.sage@gmail.com>
2022-10-12 22:58:14 +0530

    added usersetting and create blog pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 function App() {
   return (
     <>
