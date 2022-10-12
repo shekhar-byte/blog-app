b9e39a159775620fba37cb4244cd30699a32c0f9
Samardeep kajal <samk712003@gmail.com>
2022-10-12 23:58:20 +0530

    Fixed navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,26 +1,38 @@
-import "./navbar.css"
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <h2 className="topIcon">Blog<i className="fa-solid fa-store"></i></h2>
+        <h2 className="topIcon">
+          Blog<i className="fa-solid fa-store"></i>
+        </h2>
       </div>
       <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
-          <li className="topListItem">ABOUT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
+          <li className="topListItem">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/createform">CREATE</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/register">REGISTER</Link>
+          </li>
+          <li className="topListItem">
+            <Link to="/login">LOGIN</Link>
+          </li>
         </ul>
       </div>
       <div className="topRight">
-        <img className="topImage"
-          src="https://media.istockphoto.com/photos/headshot-portrait-of-smiling-ethnic-businessman-in-office-picture-id1300512215?b=1&k=20&m=1300512215&s=170667a&w=0&h=LsZL_-vvAHB2A2sNLHu9Vpoib_3aLLkRamveVW3AGeQ=" alt="" />
+        <img
+          className="topImage"
+          src="https://media.istockphoto.com/photos/headshot-portrait-of-smiling-ethnic-businessman-in-office-picture-id1300512215?b=1&k=20&m=1300512215&s=170667a&w=0&h=LsZL_-vvAHB2A2sNLHu9Vpoib_3aLLkRamveVW3AGeQ="
+          alt=""
+        />
         <i className=" topSearchIcon fa-brands fa-searchengin"></i>
       </div>
     </div>
-  )
+  );
 }
-
-
