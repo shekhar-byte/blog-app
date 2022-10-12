a0fa2674555343ee74eebd33f7ac628f48092f26
sarthak <sarthakgupta878@gmail.com>
2022-10-12 22:02:04 +0530

    login/register added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
           <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
       </Router>
