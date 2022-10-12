e1ba08d0b708af3c3e019e7d14117c4e0406ad5d
shekhar-byte <85403918+shekhar-byte@users.noreply.github.com>
2022-10-12 23:11:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Register from './pages/register/Register';
+
 function App() {
   return (
     <>
@@ -12,8 +11,6 @@ function App() {
           <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
           </Routes>
         </div>
       </Router>
