cf7cfec94f05c631be8112497f0245bfa43e6615
Samardeep kajal <samk712003@gmail.com>
2022-10-12 23:49:22 +0530

    Added all  routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
-
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import UserSetting from "./pages/userSettings/UserSetting";
+import CreateForm from "./pages/create/CreateForm";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Show from "./pages/show/Show";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
@@ -8,6 +15,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/blogpost" element={<Show />} />
+            <Route path="/setting" element={<UserSetting />} />
+            <Route path="/createform" element={<CreateForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       </Router>
