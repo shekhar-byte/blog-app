26463ad3e1b6ea7f2e67511c1dee6208d2fc4769
Samardeep kajal <samk712003@gmail.com>
2022-10-12 23:29:29 +0530

    Added single post show page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Show from "./pages/show/Show";
 function App() {
   return (
     <>
       <Router>
-        <div className="App" >
+        <div className="App">
           <Navbar />
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/blogpost" element={<Show />} />
           </Routes>
         </div>
       </Router>
